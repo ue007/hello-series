3108d4e6b3d1c8cb41014e0fcda0d14d4a70e57a
ue007 <ue007@webgpu.info>
2022-01-08 17:23:05 +0800

    feat: hello react redux

--- examples/hello-redux/src/App.tsx
+++ examples/hello-redux/src/App.tsx
@@ -1,26 +1,50 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { counterReducer } from "./reducer";
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
 				<a
 					className="App-link"
 					href="https://reactjs.org"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					Learn React
+					Hello React Redux
 				</a>
+				<img src={logo} className="App-logo" alt="logo" />
 			</header>
 		</div>
 	);
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(counterReducer);
+
+// You can use subscribe() to update the UI in response to state changes.
+// Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+// There may be additional use cases where it's helpful to subscribe as well.
+
+store.subscribe(() => console.log(store.getState()));
+
+// The only way to mutate the internal state is to dispatch an action.
+// The actions can be serialized, logged or stored and later replayed.
+store.dispatch({ type: "counter/incremented" });
+// {value: 1}
+store.dispatch({ type: "counter/incremented" });
+// {value: 2}
+store.dispatch({ type: "counter/decremented" });
+// {value: 1}
 
 export default App;
